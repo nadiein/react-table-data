1aca7e7ca399de78da90b4b8a9bb95af55df3f46
Igor Nadiein <snoozzzy7@gmail.com>
2018-03-29 01:06:48 +0300

    Master::
    GetTrprops logic for blinking row if warning key is true/flase - not ready yet; Need to figure out why rowInfo.row.*key retunin undefined

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import PropTypes from "prop-types";
 import axios from "axios";
 import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 import Item from "./components/item";
 
 export class Main extends Component {
+
     state = {
         data: [{
             _id: '',
@@ -16,7 +18,22 @@ export class Main extends Component {
             warning: '',
             players: 0,
             maxPlayers: 0
-        }]
+        }],
+        warning: []
+    }
+
+    static propTypes = {
+        data: PropTypes.shape({
+            _id: PropTypes.string,
+            type: PropTypes.string,
+            name: {
+                first: PropTypes.string,
+                last: PropTypes.string
+            },
+            warning: PropTypes.bool,
+            players: PropTypes.number,
+            maxPlayers: PropTypes.number
+        })
     }
 
     componentDidMount() {
@@ -24,39 +41,57 @@ export class Main extends Component {
             .get('app/data/data.json')
             .then(response => {
                 this.setState({
-                    data: response.data
+                    data: response.data,
+                    warning: response.data.map(el => {return el.warning})
                 });
             }); 
+    }
+
+    target = (ev) => {
+        console.log(ev.target);
+    }
+
+    getTrProps = (rowInfo) => {
+        const {warning} = this.state;
+        if (rowInfo) {
+            rowInfo.data.map(el => {console.log(el.warning)})
+            return {
+                className: rowInfo.data.map(el => {return el ? 'blinking' : ''})
+            }
+        }
+        return {};
     }
 
     render() {
         const {data} = this.state;
         const columns = [{
             Header: 'ID',
-            accessor: '_id' // String-based value accessors!
+            accessor: '_id'
         }, {
             Header: 'Type',
             accessor: 'type',
-            Cell: props => <img className='item-img' src={props.value} /> // Custom cell components!
+            Cell: props => <img className='item-img' src={props.value} />
         }, {
-            id: 'Name', // Required because our accessor is not a string
+            id: 'Name',
             Header: 'Name',
-            accessor: d => d.name.first + ' ' + d.name.last // Custom value accessors!
+            accessor: d => d.name.first + ' ' + d.name.last
         }, {
             Header: 'Players',
-            accessor: 'players' // String-based value accessors!
+            accessor: 'players'
         }, {
             Header: 'Maximum Players',
-            accessor: 'maxPlayers' // String-based value accessors!
+            accessor: 'maxPlayers'
+        }, {
+            id: 'Warning',
+            Header: 'Warning',
+            accessor: d => d.warning.toString()
         }];
 
         return (
-            <div className="d-flex align-items-center justify-content-center">
+            <div className="d-flex align-items-center justify-content-center" onClick={this.target}>
                 <ReactTable data={data} 
                             columns={columns}
-                            getTrProps={(state, rowInfo, column, instance) => ({
-                                onClick: e => console.log('A row was clicked!')
-                            })}
+                            getTrProps={this.getTrProps}
                 />
             </div>
         );
